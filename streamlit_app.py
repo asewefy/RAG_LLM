fa6ee5947f2b3700cd17487aa3a56a37d809000c
asewefy <asewefy@gmail.com>
2025-07-02 21:43:48 +0300

    Create streamlit_app.py
    Initial version.

---
+++ streamlit_app.py
@@ -0,0 +1,28 @@
+!pip install faiss-cpu --no-cache
+!pip install -q -U google-generativeai
+!pip install python-dotenv
+!pip install streamlit
+
+import streamlit as st
+
+ocr_system = OCR_RAG_System(knowledge_base_path='knowledge_base.pkl')
+#image_path = upload_file()
+#response = ocr_system.process_image_with_rag(image_path, True)
+
+st.title("Arabic Handwritten Text OCR Service")
+st.header("Upload an image file")
+image_path = st.file_uploader("Select an image file (.jpg)")
+response = ocr_system.process_image_with_rag(image_path, True)
+ocr_system.save_knowledge_base()
+
+col1, col2 = st.columns(2)
+with col1:
+  if(image_path is not None):
+    st.image(f"{image_path}_.jpg")
+with col2:
+  if(response is not None):
+    st.text(response)
+
+if(response is not None):
+  if(st.button("Read the text for me!")):
+    ReadText(response)
