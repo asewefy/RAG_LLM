aeea2a5635f0b3896f94650726d4583d01eb5f5e
asewefy <asewefy@gmail.com>
2025-07-05 23:59:09 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ ocr_system = rag.OCR_RAG_System(knowledge_base_path='knowledge_base.pkl')
 st.title("Arabic Handwritten Text OCR Service")
 st.header("Upload an image file")
 uploaded_file = st.file_uploader("Select an image file (.jpg)", type=["jpg"])
-image_path = ''
+image_path = None
 if uploaded_file is not None:
     with tempfile.NamedTemporaryFile(delete=False) as temp_file:
         temp_file.write(uploaded_file.read())
@@ -23,7 +23,7 @@ if uploaded_file is not None:
 
 col1, col2 = st.columns(2)
 with col1:
-  if(image_path is not None):
+  if(image_path is not None and os.path.exists(image_path)):
     st.image(f"{image_path}_.jpg")
 with col2:
   if(response is not None):
